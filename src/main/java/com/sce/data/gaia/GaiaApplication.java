74509b47667d24eb4bfcd549bc97b1bb1f0863d0
shisw <shisw@sce-re.com>
2018-07-09 16:49:02 +0800

    remove print springboot log

--- src/main/java/com/sce/data/gaia/GaiaApplication.java
+++ src/main/java/com/sce/data/gaia/GaiaApplication.java
@@ -1,8 +1,10 @@
 package com.sce.data.gaia;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +19,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class GaiaApplication {
 
     public static void main(String[] args) {
-        ApplicationContext ctx =  SpringApplication.run(GaiaApplication.class, args);
+        ApplicationContext ctx =   new SpringApplicationBuilder()
+                .bannerMode(Banner.Mode.OFF)
+                .sources(GaiaApplication.class)
+                .run(args);
         String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
         for (String profile : activeProfiles) {
             log.info("project is start on profile:{}" , profile);

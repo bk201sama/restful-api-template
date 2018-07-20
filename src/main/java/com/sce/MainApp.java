aa1494035e7f985c47af36ec265552791241cdb1
shisw <shisw@sce-re.com>
2018-07-20 17:19:22 +0800

    vertx.web init

--- src/main/java/com/sce/MainApp.java
+++ src/main/java/com/sce/MainApp.java
@@ -1,10 +1,15 @@
 package com.sce;
 
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 
-public class MainApp {
+public class MainApp  extends AbstractVerticle {
 
     public static void main(String... args) {
+        Vertx.vertx().createHttpServer().requestHandler(req -> req.response()
+                .putHeader("content-type", "text/plain")
+                .end("Hello from Vert.x!")).listen(8080);
     }
 
 }
